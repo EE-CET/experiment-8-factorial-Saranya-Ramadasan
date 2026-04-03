727605b681322df780edbee5a9ac4cdf6f168d21
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-03 12:21:23 +0530

    first commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 public class Factorial {
 
-    // TODO: Create a method 'public static long factorial(int n)'
-    // It should return the factorial of n.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            System.out.println(factorial(n));
+        }
+        sc.close();
+    }
 
-    
-        // TODO: Read input n
-        // TODO: Call factorial(n)
-        // TODO: Print the result
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
     
 }
